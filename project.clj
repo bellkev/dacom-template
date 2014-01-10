9a2e24d36ca545f3f221d120b2897b7ef17b0c19
Kevin <kevin.a.bell@gmail.com>
2014-01-09 20:34:50 -0800

    Cut 0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 ;   Copyright (c) 2014 Kevin Bell. All rights reserved.
 ;   See the file LICENSE for copying permission.
 
-(defproject dacom/lein-template "0.1.0-SNAPSHOT"
+(defproject dacom/lein-template "0.1.0"
   :description "A Leiningen template for the DACOM stack"
   :url "https://github.com/bellkev/dacom"
   :license {:name "Eclipse Public License"
